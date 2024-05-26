7006f01bda635a0e3ec44bcad4b14a5a4d3b4db9
fatumaahmed2024 <fatuma.mohamed@starthmore.edu>
2024-05-26 20:08:25 +0300

    Displaying the current day of the week

--- in.php
+++ in.php
@@ -7,3 +7,6 @@ print "<br>";//Using HTML <br> to break a line
 print 1999;//This is an integer or a digit
 print "<br>";//Using HTML <br> to break a line
 print "5475";//This is a string
+print "<br>";//Using HTML <br> to break a line
+print date ('1');//Displaying the current day of the week
+print "<br>";//Using HTML <br> to break a line
