0a2d1ebc708f67e1036356c48663d012e18dcfda
fatumaahmed2024 <fatuma.mohamed@starthmore.edu>
2024-05-26 20:06:23 +0300

    printed an integer as a string

--- in.php
+++ in.php
@@ -5,3 +5,5 @@ print "<br>";//Using HTML <br> to break a line
 print"Today is <span style='color: #ff4856; text-transform: uppercase;'>Thursday</span> ";
 print "<br>";//Using HTML <br> to break a line
 print 1999;//This is an integer or a digit
+print "<br>";//Using HTML <br> to break a line
+print "5475";//This is a string
