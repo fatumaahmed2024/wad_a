a0bd6e64cf32c26f7906ba62f6d3043e9ea7ec87
fatumaahmed2024 <fatuma.mohamed@starthmore.edu>
2024-07-07 16:13:39 +0300

    delete data from the database

--- view_messages.php
+++ view_messages.php
@@ -2,8 +2,22 @@
    require_once("Includes/db_connect.php"); 
   include_once ("templates/heading.php");   
   require_once ("templates/nav.php");
+
+  if(isset($GET["DelId"])){
+        $DelId=$GET["DelId"];
+
+      // sql to delete a record
+      $del_mes = "DELETE FROM messages WHERE messageId='$DelId' LIMIT 1";
+
+      if ($conn->query( $del_mes) === TRUE) {
+       header("Location: view_messages.php");
+       exit();
+      } else {
+        echo "Error deleting record: " . $conn->error;
+      }
+  }
 ?>    
-   
+
          <div class="banner">
             <h1>Messages</h1>
            </div>
@@ -38,9 +52,11 @@
                 <td><?php print $sel_msg_row["sender_name"]; ?></td>
                 <td><?php print $sel_msg_row["sender_phone_number"]; ?></td>
                 <td><?php print $sel_msg_row["sender_email"]; ?></td>
-                <td><?php print "<strong>" .$sel_msg_row["Destination"] .'</strong> - ' .substr($sel_msg_row["text_message"], 0 , 10). '...' ;?></td>
+                <td><?php print "<strong>" .$sel_msg_row["Destination"] .'</strong> - '
+                 .substr($sel_msg_row["text_message"], 0 , 10). '...' ;?></td>
                 <td><?php print date("d-M-Y H:i", strtotime( $sel_msg_row["datecreated"])); ?></td>
-                <td>[<a href="edit_msg.php?messageId=<?php print $sel_msg_row["messageId"]; ?>"> Edit</a> ] [ Del]</td>
+                <td>[<a href="edit_msg.php?messageId=<?php print $sel_msg_row["messageId"]; ?>"> Edit</a> ]
+                 [ <a href="?DelId=<?php print $sel_msg_row["messageId"]; ?>">Del</a>]</td>
         </tr>
              
 <?php    
