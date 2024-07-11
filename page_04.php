9311378fdb08dc21d777ea5168279a6ebfbaef1a
fatumaahmed2024 <fatuma.mohamed@starthmore.edu>
2024-07-11 09:24:17 +0300

    removed a session from the system

--- page_04.php
+++ page_04.php
@@ -5,7 +5,7 @@ print $_SESSION["fname"];
 
 if(isset($_SESSION["fname"])){
 
-   
+    unset($_SESSION["fname"]);//removes the session from the system
 
     header("Location: page_03.php");
     exit();
